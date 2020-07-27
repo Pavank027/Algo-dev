64862da573e581767c9f3cd9f78089f9f17368f8
Pavan <pavank027@gmail.com>
2020-07-27 17:10:17 +0530

    refactored folder name

--- demo/src/main/java/com/general/example/demo/BalanceParan/ParaenthesisBalance.java
+++ demo/src/main/java/com/general/example/demo/BalanceParan/ParaenthesisBalance.java
@@ -1,4 +1,4 @@
-package test;
+package BalanceParan;
 
 package com.general.example.demo;
 import java.util.*;
--- demo/src/test/java/com/general/example/demo/BalanceParan/ParaenthesisBalanceTest.java
+++ demo/src/test/java/com/general/example/demo/BalanceParan/ParaenthesisBalanceTest.java
@@ -1,4 +1,4 @@
-package test;
+package BalanceParan;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
